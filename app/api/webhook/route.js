83c226c8f71d8b71583848670d1095b56cf23158
Anurag Sharma <71564387+anur4ag@users.noreply.github.com>
2024-03-03 23:55:47 +0530

    push

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -54,6 +54,6 @@ export async function POST(req) {
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   console.log('Webhook body:', body)
  
-  return new Response('', { status: 200 })
+  return new Response(evt.data, { status: 200 })
 }
  
